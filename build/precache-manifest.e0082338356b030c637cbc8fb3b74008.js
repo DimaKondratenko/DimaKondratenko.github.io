f00be6120f80558c04931dcc45ceea72d0dfe5b9
Dima Kondratenko <kondratenkodima364@gmail.com>
2020-06-14 13:59:35 +0300

    added build/

---
+++ build/precache-manifest.e0082338356b030c637cbc8fb3b74008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0574a26f525e361a6805d14c3bf76f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec6f36616e120d2b8c8",
+    "url": "/static/css/main.2cb7d99d.chunk.css"
+  },
+  {
+    "revision": "53bf6351f5b5ee97a9bd",
+    "url": "/static/js/2.8ba249f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ba249f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec6f36616e120d2b8c8",
+    "url": "/static/js/main.a5abea09.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
